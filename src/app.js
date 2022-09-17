0fcbab7f46182d56a1143080573c399aa4b0807e
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-09-17 13:55:45 -0300

    fix req 1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { productsRouter } = require('./controllers');
+const { productsRouter } = require('./router');
 
 const app = express();
 app.use(express.json()); 
